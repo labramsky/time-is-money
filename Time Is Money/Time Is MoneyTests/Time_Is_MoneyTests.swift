7755fcb93916e08632885b377dbe9df9bee4448f
labramsky <labramsky@deloitte.ca>
2018-09-02 10:16:03 -0400

    Set up Xcode project

---
+++ Time Is Money/Time Is MoneyTests/Time_Is_MoneyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Time_Is_MoneyTests.swift
+//  Time Is MoneyTests
+//
+//  Created by Abramsky, Lauren (CA - Toronto) on 2018-09-01.
+//  Copyright © 2018 Lauren Abramsky. All rights reserved.
+//
+
+import XCTest
+@testable import Time_Is_Money
+
+class Time_Is_MoneyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
